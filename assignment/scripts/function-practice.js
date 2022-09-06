c34fe3b43b8f1e7fa4f2b7285f492355df08b69e
Tormod Sletteboe <tormod.slettebo@gmail.com>
2022-09-06 15:23:22 -0500

    did 1-7

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -15,30 +15,31 @@ console.log('Test - should say "Hello World!"', hello());
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
 function helloName( name ) {
-  return;
+  return 'Hello, '+name+'!';
 }
 // Remember to call the function to test
-
+console.log( helloName('tormod'));
 
 // 3. Function to add two numbers together & return the result
-function addNumbers( firstNumber ) {
+function addNumbers( firstNumber,secondNumber ) {
   // return firstNumber + secondNumber;
+  return firstNumber+secondNumber;
 }
-
+console.log(addNumbers(12,13));
 
 // 4. Function to multiply three numbers & return the result
-function multiplyThree( ){
-
+function multiplyThree(num1,num2,num3 ){
+  return num1*num2*num3;
 }
-
+console.log(multiplyThree(2,3,4));
 
 // 5. Function that will return true if a number is positive, 
 //    or greater than zero, and false otherwise
 function isPositive( number ) {
   if ( number > 0 ){
-    return;
+    return true;
   }
-    return;
+    return false;
 }
 // Call the function to test each outcome (true & false) 
 // Write a separate console.log statement for each outcome
@@ -50,15 +51,36 @@ console.log( 'isPositive - should say false', isPositive(-3) );
 // 6. Function to return the _last_ item in an array. If the 
 //    array is empty, return `undefined`.
 function getLast( array ) {
-
+  return array.pop();
 }
+let somearray = ['ding','dong','dang','dim'];
+console.log('pop last array item dim:' +getLast(somearray));
+let someEmptyArray=[];
+console.log('test empty array [], should say undefined: ' +getLast(someEmptyArray));
+
 
 // 7. Function to find a value in an array. Return true if the 
 //    value is found and false otherwise. Use a loop;
 //    DO NOT use Array.includes, Array.indexOf, or Array.find 
 function find( value, array ){
+  let arraylength = array.length;
+  for(let i=0;i<arraylength;i++){
+    if(value==array[i]){
+      return true;
+    }
+  }
   
+  
+  return false;
 }
+let arrayOfValues = [10,11,12,13,14];
+console.log('looking for 13 in array: should return true: '+find(13,arrayOfValues));
+console.log('looking for 27 in array: should return false: '+find(27,arrayOfValues));
+console.log('looking for hello in array: should return false: '+find('hello',arrayOfValues));
+let emptyArray=[];
+console.log('passing in empty array: should return false: '+find('hello',emptyArray));
+let nonArray=0;
+console.log('passing in non array: should return false: '+find('hello',nonArray));
 
 // ----------------------
 // Stretch Goals
